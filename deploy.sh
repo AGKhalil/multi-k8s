9c1d55b969a67147552b5aeb8a3843f7df76b148
khela <khela@eduroam-80-87.nomadic.bris.ac.uk>
2019-10-04 09:35:18 +0100

    travis configs added

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+docker build -t agkhalil/multi-client -f ./client/Dockerfile ./client
+docker build -t agkhalil/multi-server -f ./server/Dockerfile ./server
+docker build -t agkhalil/multi-worker -f ./worker/Dockerfile ./worker
+docker push agkhalil/multi-client
+docker push agkhalil/multi-server
+docker push agkhalil/multi-worker
+kubectl apply -f k8s
+kubectl rollout restart deployments
