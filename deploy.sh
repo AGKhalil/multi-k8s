15c0fcf2bf4d6634813e470195c0b9f992eaa537
khela <khela@eduroam-80-87.nomadic.bris.ac.uk>
2019-10-04 10:30:13 +0100

    updated deploy.sh to fit the old version of kubectl for updating

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
-docker build -t agkhalil/multi-client -f ./client/Dockerfile ./client
-docker build -t agkhalil/multi-server -f ./server/Dockerfile ./server
-docker build -t agkhalil/multi-worker -f ./worker/Dockerfile ./worker
-docker push agkhalil/multi-client
-docker push agkhalil/multi-server
-docker push agkhalil/multi-worker
+docker build -t agkhalil/multi-client:latest -t agkhalil/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t agkhalil/multi-server:latest -t agkhalil/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t agkhalil/multi-worker:latest -t agkhalil/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push agkhalil/multi-client:latest
+docker push agkhalil/multi-server:latest
+docker push agkhalil/multi-worker:latest
+
+docker push agkhalil/multi-client:SHA
+docker push agkhalil/multi-server:SHA
+docker push agkhalil/multi-worker:SHA
+
 kubectl apply -f k8s
-# kubectl rollout restart deployments
-kubectl version
+kubectl set image deployments/server-deployment server=agkhalil/multi-server:$SHA
+kubectl set image deployments/client-deployment client=agkhalil/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=agkhalil/multi-worker:$SHA
